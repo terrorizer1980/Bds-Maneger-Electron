d0df6e6a0a4d17f19a1ae59c3709b2f5c6e17b82
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-11-10 22:56:27 -0300

    teste

--- pages/assents/js/check_version.js
+++ pages/assents/js/check_version.js
@@ -1,11 +1,23 @@
-var url = 'https://raw.githubusercontent.com/Sirherobrine23/Bds_Maneger-for-Windows/main/package.json';
-fetch(url).then(response => response.text()).then(dataURLv => {var obj1 = JSON.parse(dataURLv);var latestV = obj1.version;localStorage.setItem('bds_gitv', latestV)});
-var latestVersion = localStorage.getItem('bds_gitv');
-const localV = require('electron').remote.app.getVersion()
-console.log('Ultima Versão Disponivel no repositorio: '+ latestVersion, 'Sua Versão local: '+localV)
-var check_latest_version = latestVersion >= localV
+var url_app = 'https://raw.githubusercontent.com/Sirherobrine23/Bds_Maneger-for-Windows/main/package.json';
+fetch(url_app).then(response => response.text()).then(dataURLv => {
+    var obj1 = JSON.parse(dataURLv);
+    var latestV = obj1.version;
+    const localV = require('electron').remote.app.getVersion()
+    console.log('Ultima Versão Disponivel no repositorio: '+ latestV, 'Sua Versão local: '+localV)
+    var check_latest_version = latestV >= localV
+        if (check_latest_version){
+            console.log('Voçê está na ultima versão')
+        } else {
+            if (confirm('You not have latest Version, is open in new Windows Download Pages')){
+                window.open("https://github.com/Sirherobrine23/Bds_Maneger-for-Windows/releases", 'Github Release Download Pages');
+            }
+        }
+    }
+);
 
-if (check_latest_version){
-    alert('You not have latest Version, is open in new Windows Download Pages');
-    window.open("https://github.com/Sirherobrine23/Bds_Maneger-for-Windows/releases", 'Github Release Download Pages');
-}
+var url_server = 'https://raw.githubusercontent.com/Sirherobrine23/Bds_Maneger-for-Windows/main/Server.json';
+fetch(url_server).then(response => response.text()).then(serverVURLv => {
+    var obj1 = JSON.parse(serverVURLv);
+    var serverV = obj1.latest;
+    console.log(serverV)  
+);
