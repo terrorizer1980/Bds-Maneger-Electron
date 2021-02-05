9846b1f20e711ad24fd149fa647e68c301729868
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-02-05 00:26:10 -0300

    docker vs-code and start_linux.sh update

--- start_linux.sh
+++ start_linux.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 #
-npm install
+if command -v node &>> /dev/null; then
+    if [[ -d node_modules ]];then
+        echo "We don't need to install anything"
+    else
+        echo "Installing Bds Manager dependencies"
+        npm install --no-save
+    fi
+else
+    if command -v curl &>> /dev/null;then
+        curl https://deb.nodesource.com/setup_current.x |sudo bash -
+    else
+        sudo apt install curl -y
+        curl https://deb.nodesource.com/setup_current.x |sudo bash -
+    fi
+fi
 npm run start
