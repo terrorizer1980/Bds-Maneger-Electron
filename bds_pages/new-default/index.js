c2682e6e7f5b4e0d1ff40540e35ec4209e4727ff
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-19 21:34:37 -0300

    sync Bds Maneger Core

--- bds_pages/new-default/index.js
+++ bds_pages/new-default/index.js
@@ -1,7 +1,7 @@
 function start (){
     const current_server = require("@the-bds-maneger/core").start();
     current_server.log(function (data){
-        data = "data".split(/\n/).filter(data => {if(data === "") return false; else return true}).join("<br>\n")
+        data = data.split(/\n/).join("<br>\n")
         document.getElementsByClassName("server_log")[0].innerHTML += data
     })
     current_server.exit(function (code){if (code !== 0) alert(`You Server stop with code ${code}`)})
