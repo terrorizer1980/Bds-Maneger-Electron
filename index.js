63f8e38c408c9d44c4de20598f40f55daec6cf08
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-29 22:04:48 -0300

    Push

--- index.js
+++ index.js
@@ -9,72 +9,75 @@ const bds = require("@the-bds-maneger/core");
 const { existsSync, writeFileSync, readFileSync } = require("fs");
 const uuid = require("uuid").v4
 const bodyParser = require("body-parser");
-const { tmpdir } = require("os");
+const BdsPort = (process.env.PORT || 3000)
 
-// Settings
-const SessionPathUID = resolve(tmpdir(), "BdsSessions.UUID")
-function SessionVerifify(UID){
-    try {
-        const uuid = readFileSync(SessionPathUID).toString().split("\n").join("")
-        return UID === uuid
-    } catch (error) {
-        return false
+// Register
+const UserConfig = resolve(process.cwd(), "User.json");
+if(!(existsSync(UserConfig))) writeFileSync(UserConfig, "{}")
+
+const ConfigPath = resolve(process.cwd(), "Config.json");
+var Config = {
+    register: true,
+    api: true
+};
+if(!(existsSync(ConfigPath))) writeFileSync(ConfigPath, JSON.stringify(Config, null, 4)); else Config = JSON.parse(readFileSync(ConfigPath, "utf8"))
+
+const UserRegister = resolve(process.cwd(), "Register.json");
+writeFileSync(UserRegister, "{}")
+
+console.log((function(){
+    console.log("Changing user uuids");
+    const oldUUID = JSON.parse(readFileSync(UserConfig, "utf8"));
+    const newusersfile = {};
+    for (let changeUuid of Object.getOwnPropertyNames(oldUUID)) {
+        newusersfile[uuid()] = oldUUID[changeUuid]
     }
+    writeFileSync(UserConfig, JSON.stringify(newusersfile, null, 4))
+    return "uuids successfully exchanged"
+})());
+
+
+// Functions
+function checkUser(User){
+    const config = JSON.parse(readFileSync(UserConfig, "utf8"));
+    var Status = false
+    for (let checkInfo of Object.getOwnPropertyNames(config)) {
+        const user = config[checkInfo];
+        if (user.user === User) Status = true
+    }
+    return Status
+}
+function checkPass(Pass){
+    const config = JSON.parse(readFileSync(UserConfig, "utf8"));
+    var Status = false
+    for (let checkInfo of Object.getOwnPropertyNames(config)) {
+        const user = config[checkInfo];
+        if (user.pass === Pass) Status = true
+    }
+    return Status
+}
+function getUSerUUID(user){
+    const Users = JSON.parse(readFileSync(UserConfig));
+    for (let _usS of Object.getOwnPropertyNames(Users)) {
+        Users[_usS];
+        if (Users[_usS].user === user) return _usS
+    }
+    return false
+}
+function checkUUID(uid){
+    const Users = JSON.parse(readFileSync(UserConfig));
+    for (let _usS of Object.getOwnPropertyNames(Users)) {
+        Users[_usS];
+        if (_usS === uid) return true
+    }
+    return false
 }
 
+// Express Settings
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-// URi
-function sendLog(data){
-    data = `${data}`.split("\n").filter(data =>{return (data !== "")})
-    io.emit("BdsLog", data);
-    for (let log of data) console.log(log);
-}
-
-app.get("/", (req, res) => res.redirect("/login"));
-app.get("/index/:UUID", (req, res) => {
-    const parms = req.params
-    if (!(SessionVerifify(parms.UUID))) res.redirect("/login");
-    res.sendFile(resolve(__dirname, "page", "index.html"));
-});
-
-app.post("/GetUserID", (req, res) => {
-    const body = req.body
-    const UserConfig = resolve(process.cwd(), "User.json")
-    if (!(existsSync(UserConfig))) writeFileSync(UserConfig, JSON.stringify({
-        user: uuid(),
-        pass: uuid().split("-").join("").substr(-8)
-    }, null, 4))
-    const config = JSON.parse(readFileSync(UserConfig, "utf8"))
-    
-    // Return Status
-    if ((body.pass !== config.pass)) res.redirect("/login?error=User")
-    else if (body.user !== config.user) res.redirect("/login?error=Pass")
-    else {
-        const uuidX = `${uuid().split("-").join("").substr(-(parseInt(Math.random() * 1000)))}${uuid().split("-").join("").substr(-(parseInt(Math.random() * 1000)))}${uuid().split("-").join("").substr(-(parseInt(Math.random() * 1000)))}`
-        writeFileSync(SessionPathUID, uuidX)
-        res.redirect(`/index/${uuidX}`);
-    }
-})
-
-app.get("/login", (req, res) => {res.sendFile(resolve(__dirname, "page/login.html"))})
-
-app.post("/service", (req, res) => {
-    const _h = req.headers.command;
-    const status = {
-        code: 401,
-        te: "Error"
-    }
-    if (_h === "start") {
-        const bdsstart = bds.start();
-        bdsstart.log(data => sendLog(data))
-        status["te"] = "Sucess";
-        status["code"] = 201;
-    }
-    res.send(status["te"])
-})
-
+// Socket.io
 io.on("connection", (socket) => {
     console.log(`User Id Connected: ${socket.id}`);
     socket.on("disconnect", ()=>{
@@ -82,7 +85,114 @@ io.on("connection", (socket) => {
     })
 });
 
-app.get("*", (req, res) => res.redirect("/login"));
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+function sendLog(data){
+    data = `${data}`.split("\n").filter(data =>{return (data !== "")})
+    io.emit("BdsLog", data);
+    for (let log of data) console.log(log);
+}
+
+// URi
+app.get("/", (req, res) => res.redirect("/login"));
+
+app.get("/index", (req, res)=>{
+    const body = req.query
+    if (checkUUID(body.uuid)) {
+        var Index = readFileSync(resolve(__dirname, "page/index.html"), "utf8").toString();
+        Index = Index.split("@UUID").join(body.uuid)
+        return res.send(Index)
+    } else res.redirect("/login")
+})
+
+app.get("/logout", (req, res)=>{
+    const _uuid = req.query.uuid
+    if (checkUUID(_uuid)) {
+        const users = JSON.parse(readFileSync(UserConfig, "utf8"))
+        if (users[_uuid]) {
+            const newuid = uuid()
+            users[newuid] = users[_uuid]
+            if (users[newuid]) delete users[_uuid]; else return res.sendStatus(501);
+            writeFileSync(UserConfig, JSON.stringify(users, null, 4));
+            res.redirect("/login")
+        }
+    } else res.sendStatus(502)
+})
+
+app.post("/GetUserID", (req, res) => {
+    const body = req.body
+    // Return Status
+    if (checkUser(body.user) && checkPass(body.pass)) res.redirect(`/index?uuid=${getUSerUUID(body.user)}`);
+    else res.redirect("/login?error=L1");
+})
+
+if (Config.register){
+    app.get("/register", (req, res) => {
+        const UserUUID = uuid();
+        const Users = JSON.parse(readFileSync(UserRegister, "utf8"));
+        if (Users[UserUUID]) return res.redirect("/register");
+        else Users[UserUUID] = {
+            user: null,
+            pass: null
+        };
+        writeFileSync(UserRegister, JSON.stringify(Users, null, 4));
+        return res.redirect(`/register/${UserUUID}`)
+    })
+
+    app.get("/register/:ID", (req, res) => {
+        const parm = req.params
+        const Users = JSON.parse(readFileSync(UserRegister, "utf8"));
+        if (!(Users[parm.ID])) return res.redirect("/register");
+        return res.sendFile(resolve(__dirname, "page/register.html"))
+    })
+    app.post("/registerSave", (req, res) => {
+        const body = req.body
+        const Register = JSON.parse(readFileSync(UserRegister, "utf8"));
+        const configUSer = JSON.parse(readFileSync(UserConfig, "utf8"));
+        if (!(Register[body.uuid])) return res.redirect("/login");
+        const User = {
+            user: body.user,
+            pass: body.pass
+        }
+        // Check
+        if (User.user.length <= 5) return res.redirect(`/register/${body.uuid}?error=15`)
+        if (User.pass.length <= 7) return res.redirect(`/register/${body.uuid}?error=16`)
+        if (checkUser(User.user)) return res.redirect(`/register/${body.uuid}?error=A`)
+        
+        delete Register[body.uuid]
+        writeFileSync(UserRegister, JSON.stringify(Register, null, 4))
+        
+        configUSer[body.uuid] = User
+        writeFileSync(UserConfig, JSON.stringify(configUSer, null, 4));
+        res.redirect(`/index?uuid=${body.uuid}`)
+    })
+} else {
+    app.get("/register", (req, res) => {
+        res.redirect("/login", 302)
+    })
+}
+app.get("/login", (req, res) => {res.sendFile(resolve(__dirname, "page/login.html"))})
+
+// Bds Core Bridge
+app.post("/service", (req, res) => {
+    const _h = req.headers.command;
+    const _uuid = req.headers.uuid;
+    // Check uuid session
+    if (!(checkUUID(_uuid))) return res.sendStatus(401);
+    if (_h === "start") {
+        const bdsstart = bds.start();
+        bdsstart.log(data => sendLog(data))
+        return res.send(bdsstart.uuid)
+    } else if (_h === "stop") {
+        const JsS = {
+            exe: global.BdsExecs,
+            uuid: _uuid,
+            bdscoreUuid: req.headers["bdscoreuuid"],
+            V2: req.headers
+        }
+        return res.send(global.BdsExecs[JsS.bdscoreUuid].stop())
+    }
+    return res.statusCode(401)
+})
+
+server.listen(BdsPort, () => {
+  console.log(`listening on *:${BdsPort}`);
 });
